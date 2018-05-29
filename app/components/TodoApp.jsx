764a357c174c9940612ccce0c5b09722b846987f
runsf <mclausaudio@gmail.com>
2018-05-29 02:37:33 +0000

    added testing

--- app/components/TodoApp.jsx
+++ app/components/TodoApp.jsx
@@ -1,8 +1,35 @@
 var React = require('react');
+var TodoList = require('TodoList')
 
 var TodoApp = React.createClass({
+    getInitialState: function () {
+        return {
+            todos: [
+                {
+                    id: 1,
+                    text: "Get soap from farmer's market"
+                }, {
+                    id: 2,
+                    text: "Walk to summit hill"
+                }, {
+                    id: 3,
+                    text: "Study react"
+                }, {
+                    id: 4,
+                    text: "text cassidy"
+                }
+            ]
+        }
+            
+    },
     render: function () {
-        return <p>TodoApp.jsx</p>;
+        var {todos} = this.state;
+        
+        return (
+         <div>
+            <TodoList todos={todos}/>
+         </div>   
+        )
     }
 });
 
