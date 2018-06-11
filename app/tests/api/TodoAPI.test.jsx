d39296e86d49d7cd331b5f2289eb17c460814ab9
runsf <mclausaudio@gmail.com>
2018-06-11 18:56:33 +0000

    added search feature

--- app/tests/api/TodoAPI.test.jsx
+++ app/tests/api/TodoAPI.test.jsx
@@ -53,5 +53,48 @@ describe('TodoAPI', () => {
         
     });
     
+    describe('filteredTodos',() => {
+        var todos = [{
+            id: 1,
+            text: 'some text',
+            completed: true
+        }, {
+            id: 2,
+            text: 'more text',
+            completed: false
+        }, {
+            id: 3,
+            text: 'and even some more stuff',
+            completed: true
+        }]
+        
+        it('should return all items if showCompleted is true', () => {
+            var filteredTodos = TodoAPI.filterTodos(todos, true, '');
+            expect(filteredTodos.length).toBe(3);
+        })    
+        
+        it('should return uncompleted items if showCompleted is false', () => {
+            var filteredTodos = TodoAPI.filterTodos(todos, false, '');
+            expect(filteredTodos.length).toBe(1);
+        })           
+        
+        it('should sort by compelted status', () => {
+            var filteredTodos  = TodoAPI.filterTodos(todos, true, '');
+            
+            expect(filteredTodos[0].completed).toBe(false);
+        });
+        
+        it('should filter todo by search text', () => {
+            var filteredTodos = TodoAPI.filterTodos(todos, true, 'text');
+            expect(filteredTodos.length).toBe(2);
+        })
+        
+        it('should return all todos if searchText is empty', () => {
+            var filteredTodos = TodoAPI.filterTodos(todos, true, '');
+            expect(filteredTodos.length).toBe(3);
+        })
+        
+    })
+    
 })
 
