e307cc8e17862893a593d07d26ac21c3023cf688
runsf <mclausaudio@gmail.com>
2018-07-10 03:24:13 +0000

    added todos reducer and tests

--- app/tests/reducers/reducers.test.jsx
+++ app/tests/reducers/reducers.test.jsx
@@ -29,4 +29,38 @@ describe('Reducers', () => {
         })
     })
     
+    describe('todosReducer', ()=>{
+        it('it should add new todo', () => {
+            var action = {
+                type: 'ADD_TODO',
+                text: "walk dog"
+            };
+            
+            var res = reducers.todosReducer([], action);
+            
+            expect(res.length).toEqual(1);
+            expect(res[0].text).toEqual(action.text);
+        })
+        
+        it('should toggle todo', () => {
+            var todos = [{
+                id: '123',
+                text: 'Something',
+                completed: true,
+                createdAt: 123,
+                completedAt: 125
+            }]
+            
+            var action = {
+                type: 'TOGGLE_TODO',
+                id: '123'
+            };
+        var res = reducers.todosReducer(df(todos), df(action));
+        
+        expect(res[0].completed).toEqual(false);
+        expect(res[0].completedAt).toEqual(undefined);
+            
+        })
+        
+    })
 })
